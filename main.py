49e1d55907430af1b68b1fb47dfb373a211ed36b
Alif Arlendi <139869958+alifarlendi@users.noreply.github.com>
2025-02-14 23:35:31 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # Python Banking Program
 
-def show_balance():
+def show_balance(balance):
     print(f"Your balance is ${balance:.2f}")
 
 def deposit():
@@ -8,33 +8,49 @@ def deposit():
 
     if amount < 0:
         print("That's not a valid amount!")
+        return 0
     else:
         return amount
 
-def withdraw():
-    pass
+def withdraw(balance):
+    amount = float(input("Enter amount to be withdrawn: "))
 
-balance = 0
-is_running = True
-
-while is_running:
-    print("Simple Banking Program")
-    print("1. Show Balance")
-    print("2. Deposit")
-    print("3. Withdraw")
-    print("4. Exit")
-
-    choice = input("Enter your choice (1-4): ")
-
-    if choice == '1':
-        show_balance()
-    elif choice == '2':
-        balance += deposit()
-    elif choice == '3':
-        withdraw()
-    elif choice == '4':
-        is_running = False
+    if amount > balance:
+        print("Insufficient funds")
+        return 0
+    elif amount < 0:
+        print("Amount must be greater than 0")
+        return 0
     else:
-        print("That is not a valid choice!")
+        return amount
 
-print("Thank you! Have a nice day!")
+
+def main():
+    balance = 0
+    is_running = True
+
+    while is_running:
+        print("**************************")
+        print("  Simple Banking Program  ")
+        print("1. Show Balance")
+        print("2. Deposit")
+        print("3. Withdraw")
+        print("4. Exit")
+
+        choice = input("Enter your choice (1-4): ")
+
+        if choice == '1':
+            show_balance(balance)
+        elif choice == '2':
+            balance += deposit()
+        elif choice == '3':
+            balance -= withdraw(balance)
+        elif choice == '4':
+            is_running = False
+        else:
+            print("That is not a valid choice!")
+
+    print("Thank you! Have a nice day!")
+
+if __name__ == '__main__':
+    main()
